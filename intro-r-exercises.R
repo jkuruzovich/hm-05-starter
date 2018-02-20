985a6040e92d055d5fc84965f30f0aabb193be39
Jason Kuruzovich <jkuruzovich@gmail.com>
2018-02-19 19:59:33 -0500

    Update R and tests.

--- intro-r-exercises.R
+++ intro-r-exercises.R
@@ -1,9 +1,9 @@
 
 # ## Exercises
 
+#Start off by setting your working directory to the repository. For example
+setwd("~/githubdesktop/0_class/hm-05-jkuruzovich")
 
-# Note this is easily viewed from the [github repository](https://github.com/techfundamentals-fall2017/hm-05-starter).
-#
 # For the exercise below you should import the Iris Dataset as a dataframe called iris.df.
 #
 # (1). Create a variable called sepal_width.mean that contains the mean of the sepal_width column of iris.df.
@@ -20,28 +20,43 @@
 #
 # (6). While we can submit our answer to Kaggle to see how it will perform, we can also utilize our test data to assess accuracy. Accuracy is the percentage of predictions made correctly-i.e., the percentage of people in which our prediction regarding their survival.
 #
-# a. Create columns in the training dataset PredEveryoneDies and PredGender with the same predictions from above.
+# a. Create columns in the training dataset PredEveryoneDies and PredGender with the same predictions as we had for the Python version.  For example, PredEveryoneDies should have a value of 0 throughout and PredGender should have a value of 1 for women and 0 for men.
 #
-# b. Create variables AccEveryoneDies and AccGender using a calculation of accuracy of predictions for the training dataset.
+# b. Create variables AccEveryoneDies and AccGender using a calculation of accuracy of predictions for the training dataset for the respective columns.
 #
 # (7). Notice how we are utilizing the code to select out the passengerID and the Survived column and generating a submission file over and over? This is in need of a function. Create a generate_submission function that accepts a DataFrame, a target column, and a filename and writes out the submission file with just the passengerID and the Survived columns, where the survived column is equal to the target column. It should then return a DataFrame with the passengerID and the Survived columns.
 #
-# Executeing the following:
+# Executing the following:
 #
 #   `submitdie <- generate_submission(train, "PredEveryoneDies", "submiteveryonedies.csv")`
 #
 # Should return a dataframe with just passengerID and the Survived column. (You will have to execute this to pass tests.)
 #
-# (8). In according to the women and children first protocol we hypothesize that our model could be improved by including whether the individual was a child in addition to gender. After coding survival based on gender, update your recommendation to prediction in the training dataset survival based on age. `train$PredGenderAge13` should be the prediction incorporating both Gender and whether Age < 13. train$PredGenderAge18 should be the prediction incorporating both Gender and whether Age < 18. AccGenderAge13 should be the accuracy of the age prediction, based on `train$PredGenderAge13`. `AccGenderAge18` should be the accuracy of the age prediction, based on `train$PredGenderAge18`.
+# (8). In accordance with the women and children first protocol, we hypothesize that our model could be improved by including whether the individual was a child in addition to gender. After coding survival based on gender, update your recommendation to prediction in the training dataset survival based on age.
+
+# **Before doing operations on the Age column though, just go ahead and replace all NAs with the mean age for the column.**
 #
-# **Hint: You might find that you have to remove the NA's before doing operations on the Age column.**
+
+#`train$PredGenderAge13` should be the prediction incorporating both Gender and whether Age < 13. train$PredGenderAge18 should be the prediction incorporating both Gender and whether Age < 18.
+
+#`AccGenderAge13` should be the accuracy of the age prediction, based on `train$PredGenderAge13`. `AccGenderAge18` should be the accuracy of the age prediction, based on `train$PredGenderAge18`.
 #
-# (9). You should find that the AccGenderAge13 is better than AccGenderAge18. Create a new `column` child in the test and train DataFrames that is 1 if Age < 13 and 0 otherwise. This is a feature.
+
+# (9). You should find that specifying a child at a cutoff of 13 performs better than 18. Create a new `column` child in the test and train DataFrames that is 1 if Age < 13 and 0 otherwise. This is an example of a feature.
 #
 # ## Running Tests
-# Execute the following to run the tests.  You should have run all code so that necessary variables are in memory.
-install.packages("testthat")
-sink("test.md")
+# If using the anaconda version, install with:
+# conda install -c r r-testthat #run from terminal/Anaconda prompt.
+# If you downloaded Rstudio directly use:
+#install.packages("testthat")  #Run in R terminal.
+
+
+#This will load the library.
 library('testthat')
+#This will run the tests in the console for review.
+test_file("test.intro-r-exercises.R", reporter = "tap")
+
+#This will generate a file with the output from the tests. You should be sure to test and submit the commit tests.md file.
+sink("tests.md")
 test_file("test.intro-r-exercises.R", reporter = "tap")
 sink()
